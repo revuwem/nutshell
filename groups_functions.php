28694534fecd36b90bf32cdb2cde3fd30b8c3739
revuwem <revuwem@gmail.com>
2019-06-08 23:40:39 +0300

    check get_group_participnts

--- groups_functions.php
+++ groups_functions.php
@@ -114,20 +114,78 @@ function get_group_participants($connect, $group_id){
 
         $output='';
 
-        $query="SELECT * FROM `chat_groups_participants` WHERE chat_group_id=?
+
+        $query="SELECT relation_type FROM `chat_groups_participants` WHERE user_id=? and chat_group_id=?";
+        $statement=$connect->prepare($query);
+        $statement->execute(array($_SESSION['user_id'], $group_id));
+        $result_user_relation=$statement->fetchAll();
+
+
+        $query="SELECT * FROM `chat_groups_participants` WHERE chat_group_id=? order by relation_type
         ";
 
         $statement=$connect->prepare($query);
         $statement->execute(array($group_id));
         $result=$statement->fetchAll();
+       
 
-        foreach($result as $row){
-            $output .= '<tr>
-                <td width="70%"><img class="avatar rounded-circle" src="'.get_user_photo($row['user_id'], $connect).'"> '.get_user_name($row['user_id'], $connect).'</td> 
-                <td width="30%"><br><button class="btn btn-danger btn-xs drop_user_from_chat" type="button" data-deleteduserid="'.$row['user_id'].'">Удалить</button></td>
-            </tr>
-            ';
+        foreach($result_user_relation as $user_relation)
+        {
+            //Если пользователь создатель группы - он может добавлять и удалять участников
+        //Иначе пользователь может только просмотреть список участников
+            if($user_relation["relation_type"]==1)
+            {
+                $output='<tr>
+                    <td colspan="2">
+                        <div class="container-fluid input-group">
+                            <input type="text" class="form-control" name="search_new_group_participants" id="search_new_group_participants" placeholder="Поиск контакта...">
+                            <div class="input-group-append">
+                                <button class="btn form-control" id="btn_cancel_search_new_group_participants"><span class="fa fa-search fa-fw"
+                    aria-hidden="true"></span></button>
+                             </div>
+                        </div>
+                    <td>
+                </tr>';
+
+                foreach($result as $row){            
+                    $output .= '<tr>
+                        <td width="70%"><img class="avatar rounded-circle" src="'.get_user_photo($row['user_id'], $connect).'"> '.get_user_name($row['user_id'], $connect).' ';
+                    
+                    if($row["relation_type"]==1){
+                        $output .= '<p class="font-weight-light small"><i>Создатель</i></p>';
+                    };
+
+                    $output .= '</td>';
+                
+                    if($row['user_id']==$_SESSION["user_id"])
+                    {
+                        $output .= '<td width="30%"><br><button class="btn btn-danger btn-xs drop_user_from_chat" type="button" data-deleteduserid="'.$row['user_id'].'">Выйти</button></td>
+                        ';
+                    }
+                    else{
+                        $output .= '<td width="30%"><br><button class="btn btn-danger btn-xs drop_user_from_chat" type="button" data-deleteduserid="'.$row['user_id'].'">Удалить</button></td>
+                        ';
+                    };
+                    $output .= '</tr>';
+                };            
+            }
+            else{
+                foreach($result as $row){            
+                    $output .= '<tr>
+                        <td width="100%"><img class="avatar rounded-circle" src="'.get_user_photo($row['user_id'], $connect).'"> '.get_user_name($row['user_id'], $connect).' ';  
+                        
+                    if($row["relation_type"]==1){
+                        $output .= '<p class="font-weight-light small"><i>Создатель</i></p>';
+                    };
+
+                    $output .= '</td></tr>';
+                };
+            };
+
         };
+        
+              
+        
         
         echo $output;
     }
@@ -198,4 +256,3 @@ switch($_POST["action"])
     case 'update_group_name': update_group_name($connect); break;
 }
 ?>
-
